b228da4a826d457b37c3014c81f748dd8ef608b3
Bertil Tandayamo <bertilt4@gmail.com>
2025-04-19 12:22:16 -0500

    Add image hero to next cache

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['res.cloudinary.com'], // Add your image domains here if needed
+    domains: ['res.cloudinary.com','images.pexels.com'], // Add your image domains here if needed
   },
   optimizeFonts: false, // Disable font optimization to prevent timeout issues
 };
